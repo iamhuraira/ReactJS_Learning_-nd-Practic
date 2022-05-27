9559cb6ca6d3801d7d28db0c68828772843a02b3
Abu Huraira <iamhuraira429@gmail.com>
2022-05-27 22:49:29 +0500

    practice on class based components and function

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,11 +1,26 @@
-import './App.css';
+
+import React from 'react';
+import Header from './components/Header';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     
+      <Header    />
     </div>
   );
 }
 
+
+// Class Based Component
+// class App extends React.Component{
+//   render(){
+//     return (
+//       <div>
+//         <h1>Huraira</h1>
+//         <Header />
+//       </div>
+//     );
+//   }
+// }
 export default App;

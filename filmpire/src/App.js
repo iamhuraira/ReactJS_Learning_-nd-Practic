5326d89644226803969d98a168b443697da01d77
Abu Huraira <iamhuraira429@gmail.com>
2022-05-29 23:12:17 +0500

    complete movie app

--- filmpire/src/App.js
+++ filmpire/src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+import { useEffect , useState} from 'react';
 import './App.css';
-
+import search from './search.svg';
+import MovieCard from './MovieCard';
 function App() {
-  // cc1d56bd movies api key 
+  // cc1d56bd movies api key
+  const [movies, setmovies] = useState([]);
+  const [searchTerm, setsearchTerm] = useState('');
+  const Api_Url = 'http://www.omdbapi.com?apikey=cc1d56bd';
+
+  // const movie1 = 
+  //     {
+  //   "Title": "Avengers: Infinity War",
+  //   "Year": "2018",
+  //   "imdbID": "tt4154756",
+  //   "Type": "movie",
+  //   "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+  //   }
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${Api_Url}&s=${title}`);
+    const data = await response.json();
+    setmovies(data.Search);
+  };
+  useEffect(() => {
+    searchMovies('Avengers');
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search for Movie"
+          value={searchTerm}
+          onChange={(e) => setsearchTerm(e.target.value)}
+        />
+        <img src={search} alt="search icon" onClick={() => searchMovies(searchTerm)} />
+      </div>
+      {movies.length>0 ? 
+      (<div className="container">
+        {movies.map((movie)=>(
+       <MovieCard movie={movie} />
+        ))}
+      </div>) :
+        (
+      <div className='empty'>
+        <h2>No Movies Found</h2>
+      </div>
+        )
+      }
     </div>
   );
 }
---
+++ filmpire/src/MovieCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const MovieCard = ({movie}) => {
+  return (
+    <div>
+      <div className="movie">
+        <div>
+          <p>{movie.Year}</p>
+        </div>
+        <div>
+          <img
+            src={
+              movie.Poster !== 'N/A'
+                ? movie.Poster
+                : 'https://via.placeholder.com/400'
+            }
+            alt=""
+          />
+        </div>
+        <div>
+          <span>{movie.Type}</span>
+          <h3>{movie.Title}</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
